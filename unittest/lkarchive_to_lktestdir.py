18333261d5722dd8463f9198a5da14c015900daf
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-20 04:09:03 +0000

    Adding tarfile member sanitization to extractall()

--- unittest/lkarchive_to_lktestdir.py
+++ unittest/lkarchive_to_lktestdir.py
@@ -19,7 +19,26 @@ kernel_name = os.path.basename(kernel_archive[:kernel_archive.index(".tar")])
 if os.path.exists(kernel_name):
     shutil.rmtree(kernel_name)
 with tarfile.open(kernel_archive) as f:
-    f.extractall()
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
+        
+    
+    safe_extract(f)
 
 # remove all the uneccessary files
 for root, dirs, files in os.walk(kernel_name, topdown=False):
